163eda14f6826ef7ee1583d8abe0f48eee1a9416
이현주 Hyeonju Lee <tangled6648@gmail.com>
2022-11-24 00:11:07 +0900

    Delete jpa-exercise directory

--- jpa-exercise/src/main/java/com/example/jpaexercise/repository/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.jpaexercise.repository;
-
-import com.example.jpaexercise.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
